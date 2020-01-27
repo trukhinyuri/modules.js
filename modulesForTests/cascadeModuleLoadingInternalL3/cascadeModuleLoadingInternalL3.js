67f016e7c94e66c5cf40e82c5e7517e62760ce95
Yuri Trukhin <yuri@trukhin.com>
2020-01-27 12:58:00 +0300

    Added messages between module logic

--- modulesForTests/cascadeModuleLoadingInternalL3/cascadeModuleLoadingInternalL3.js
+++ modulesForTests/cascadeModuleLoadingInternalL3/cascadeModuleLoadingInternalL3.js
@@ -4,3 +4,10 @@ Modules.Loader.getModuleShadowDomRoots("cascadeModuleLoadingInternalL3").forEach
    let test = documentFragment.querySelectorAll("."+ "test")[0];
    test.innerHTML = "NewL3!";
 });
+
+function receiveTestMessage(e) {
+      alert(e.detail.message);
+
+}
+Modules.Events.Messages.subscribe("test", receiveTestMessage, "d", "cascadeModuleLoadingInternalL3");
+Modules.Events.Messages.send("moduleLoaded", "cascadeModuleLoadingInternalL3", "cascadeModuleLoadingInternalL3", "d");
--- playground.js
+++ playground.js
@@ -2,6 +2,14 @@ import {Modules} from "./modules.js";
 
 
 let run = function () {
-    Modules.Loader.loadModuleInClass(null, "./modulesForTests/", "cascadeModuleLoading", "pageHeader")
+    Modules.Loader.loadModuleInClass(null, "./modulesForTests/", "cascadeModuleLoading", "pageHeader");
+
+
+    function receiveTestMessage(e) {
+        Modules.Events.Messages.send("test", "WOW!!!", "d", "cascadeModuleLoadingInternalL3");
+
+    }
+    Modules.Events.Messages.subscribe("moduleLoaded", receiveTestMessage, "cascadeModuleLoadingInternalL3", "d");
+
 };
 Modules.Events.addStartupListener(run);
